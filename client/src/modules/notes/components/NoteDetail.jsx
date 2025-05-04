1098e4faed2a040bb8f982227d400e92abd37f32
Patrik Váňa <p-vana@seznam.cz>
2025-05-04 20:49:11 +0200

    zkouzka

--- client/src/modules/notes/components/NoteDetail.jsx
+++ client/src/modules/notes/components/NoteDetail.jsx
@@ -5,7 +5,7 @@ import "../styles/NoteDetail.css";
 const NoteDetail = ({ note, onClose }) => {
   return (
     <div className={`note-detail ${note ? "open" : ""}`}>
-      {/* Zavírací tlačítko */}
+      
       <button className="close-btn" onClick={onClose}>
         <X size={24} />
       </button>
